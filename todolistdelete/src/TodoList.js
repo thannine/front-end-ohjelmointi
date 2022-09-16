360c4668a0a2b1930e988e0de53165bcae0688c2
Tuomo Hänninen <tuomo.hanninen@myy.haaga-helia.fi>
2022-09-16 18:12:39 +0300

    TodoList with delete functionality

---
+++ todolistdelete/src/TodoList.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+function TodoList() {
+  const [todo, setTodo] = useState({desc: '', date: ''});
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  };
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+  };
+
+  const deleteTodo = (index) => {
+    setTodos((todos) => todos.filter((todo, i) => i !== index)); 
+  };
+
+  return (
+    <div>
+      <p>Add todo:</p>
+      <form onSubmit={addTodo}>
+        Description: <input type="text" name="desc" onChange={inputChanged} value={todo.desc}/>
+        Date:<input type="date" name="date" onChange={inputChanged} value={todo.date}/>
+        <input type="submit" value="Add" onClick={addTodo} />
+      </form>
+      <table>
+        <thead><tr><th>Date</th><th>Description</th></tr></thead>
+        <tbody>
+          {
+            todos.map((todo, index) => <tr key={index}>
+                <td>{todo.date}</td>
+                <td>{todo.desc}</td>
+                <td><button onClick={() => deleteTodo(index)}>Delete</button></td></tr>)
+          }
+        </tbody>
+      </table>   
+    </div>
+  );
+};
+
+export default TodoList;
