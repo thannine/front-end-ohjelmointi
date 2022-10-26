f79c0988211048dd7cf1acb6f3e1eec8efe8141f
Tuomo Hänninen <tuomo.hanninen@myy.haaga-helia.fi>
2022-10-26 19:02:56 +0300

    GraphQL

---
+++ graphql/src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import {  useQuery,  gql,} from"@apollo/client";
+
+function App() {
+
+  const GET_COUNTRIES = gql`
+    query GetCountries 
+      {    
+        countries 
+        { 
+          code,
+          name,
+          continent 
+          {
+            name
+          }  
+        }  
+      }`;
+
+  const { loading, error, data } = useQuery(GET_COUNTRIES);    
+
+  if (loading) return<p>Loading...</p>;
+  else if (error) return<p>Error...</p>;
+  else {
+    return (
+      <div className="App">
+        <table>
+        <thead><tr><th>Code</th><th>Country</th><th>Continent</th></tr></thead>
+          <tbody>
+            {
+              data.countries.map((country, index) =>
+                <tr key={index}>
+                  <td>{country.code}</td>
+                  <td>{country.name}</td>
+                  <td>{country.continent.name}</td>
+                </tr>
+              )}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
